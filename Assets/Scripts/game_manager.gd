59d111b42751dea9e0ae9d9ad2ff1e0ffbc24772
harunashi75 <harunashi75@gmail.com>
2025-04-26 00:54:50 -0400

    Pre Release v1.2.0

--- Assets/Scripts/game_manager.gd
+++ Assets/Scripts/game_manager.gd
@@ -5,10 +5,12 @@ extends Node
 
 var game_timer := 0.0
 var timer_active := true
+var timer_paused := false
 
 func _ready():
 	game_timer = 0.0
 	timer_active = true
+	timer_paused = false
 
 func _process(delta):
 	if timer_active:
@@ -16,16 +18,26 @@ func _process(delta):
 		update_timer_display()
 
 func update_timer_display():
-	var minutes = int(game_timer) / 60
-	var seconds = int(game_timer) % 60
+	var minutes := int(floor(game_timer / 60))
+	var seconds := int(game_timer) % 60
 	timer_label.clear()
 	timer_label.append_text("%02d:%02d" % [minutes, seconds])
 
 func stop_timer():
 	timer_active = false
-	var minutes = int(game_timer) / 60
-	var seconds = int(game_timer) % 60
+	timer_paused = false
+	var minutes := int(floor(game_timer / 60))
+	var seconds := int(game_timer) % 60
 	print("Temps total : %02d:%02d" % [minutes, seconds])
+
+func pause_timer():
+	timer_active = false
+	timer_paused = true
+
+func resume_timer():
+	if timer_paused:
+		timer_active = true
+		timer_paused = false
 
 func toggle_pause():
 	if pause_menu.visible:
@@ -38,11 +50,21 @@ func toggle_pause():
 func _unhandled_input(event):
 	if event is InputEventKey and event.pressed:
 		match event.keycode:
+			KEY_2: teleport_player(Vector2(-475, 0.0))
+			KEY_3: teleport_player(Vector2(208, -576))
+			KEY_4: teleport_player(Vector2(560, -48))
+			KEY_5: teleport_player(Vector2(247, 416))
+			KEY_6: teleport_player(Vector2(-855, -560))
+			KEY_7: teleport_player(Vector2(797, -576))
+			KEY_8: teleport_player(Vector2(-632, 272))
+			KEY_9: teleport_player(Vector2(615, 592))
 			KEY_KP_1: teleport_player(Vector2(-418, 1328))
 			KEY_KP_2: teleport_player(Vector2(616, 1056))
 			KEY_KP_3: teleport_player(Vector2(1017, 720))
 			KEY_KP_4: teleport_player(Vector2(-8.0, 1168))
 			KEY_KP_5: teleport_player(Vector2(-1031, 1200))
+			KEY_KP_6: teleport_player(Vector2(865, 1744))
+			KEY_KP_7: teleport_player(Vector2(246, 1744))
 
 func teleport_player(position: Vector2):
 	var player = get_node_or_null("/root/Main/Game/Player")
--- Assets/Scripts/tp_secret.gd
+++ Assets/Scripts/tp_secret.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 @export var teleport_position: Vector2  # Position où téléporter le joueur
-@onready var victory_sound = $VictorySound
 @onready var anim = $AnimatedSprite2D
 
 func _ready():
@@ -13,9 +12,8 @@ func _on_body_entered(body):
 		var player = body
 		player.reset_health()
 		player.global_position = teleport_position
-		victory_sound.play()
 
 		# Appelle stop_timer() depuis Main si disponible
-		var manager = get_node_or_null("/root/Main")
-		if manager:
-			manager.pause_timer()  # ← met juste en pause
+		var game_manager = get_node_or_null("/root/Main")
+		if game_manager and game_manager.has_method("stop_timer"):
+			game_manager.stop_timer()
